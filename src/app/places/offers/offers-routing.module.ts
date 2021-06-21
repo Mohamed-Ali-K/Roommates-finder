de27009487f2132d3ee37650c5e165963ca87dc1
Mohamed-Ali-K <kenis.mohamed.ali@gmail.com>
2021-06-21 19:20:48 +0100

    add edit offer page

--- src/app/places/offers/offers-routing.module.ts
+++ src/app/places/offers/offers-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
   {
     path: 'new-offer',
     loadChildren: () => import('./new-offer/new-offer.module').then( m => m.NewOfferPageModule)
+  },
+  {
+    path: 'edit-offer',
+    loadChildren: () => import('./edit-offer/edit-offer.module').then( m => m.EditOfferPageModule)
   }
 ];
 
