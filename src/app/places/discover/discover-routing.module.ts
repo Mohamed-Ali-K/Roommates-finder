668c8b91f6b5fbf1d890f68cdc5c145b58dd5705
Mohamed-Ali-K <kenis.mohamed.ali@gmail.com>
2021-06-21 19:23:12 +0100

    add place detail page

--- src/app/places/discover/discover-routing.module.ts
+++ src/app/places/discover/discover-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
   {
     path: '',
     component: DiscoverPage
+  },
+  {
+    path: 'place-detail',
+    loadChildren: () => import('./place-detail/place-detail.module').then( m => m.PlaceDetailPageModule)
   }
 ];
 
