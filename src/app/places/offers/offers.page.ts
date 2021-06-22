c674602534a6d2b8f3929fea5e017206768208c5
Mohamed-Ali-K <kenis.mohamed.ali@gmail.com>
2021-06-23 00:05:12 +0100

    add slinding  item to offer page

--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -1,18 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { Place } from '../place.model';
+import { IonItemSliding } from '@ionic/angular';
+import { Router } from '@angular/router';
+
 import { PlacesService } from '../places.service';
+import { Place } from '../place.model';
 
 @Component({
   selector: 'app-offers',
   templateUrl: './offers.page.html',
-  styleUrls: ['./offers.page.scss'],
+  styleUrls: ['./offers.page.scss']
 })
 export class OffersPage implements OnInit {
   offers: Place[];
-  constructor(private placesService: PlacesService) { }
+
+  constructor(private placesService: PlacesService, private router: Router) {}
 
   ngOnInit() {
     this.offers = this.placesService.places;
   }
 
+  onEdit(offerId: string, slidingItem: IonItemSliding) {
+    slidingItem.close();
+    this.router.navigate(['/', 'places', 'tabs', 'offers', 'edit', offerId]);
+    console.log('Editing item', offerId);
+  }
 }
