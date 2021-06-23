930a9406db8b14b34270b4ac2105938df4e671ac
Mohamed-Ali-K <kenis.mohamed.ali@gmail.com>
2021-06-23 23:55:23 +0100

    Add Validation to auth

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
 import { AuthService } from './auth.service';
@@ -30,5 +31,8 @@ export class AuthPage implements OnInit {
           this.router.navigateByUrl('/places/tabs/discover');
         }, 1500);
       });
+  }
+  onSubmit(form: NgForm) {
+    console.log(form);
   }
 }
