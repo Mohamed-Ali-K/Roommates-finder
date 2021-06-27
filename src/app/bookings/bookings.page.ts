fef9800e50b2960fc512a555f4511fb47cd9c0d9
Mohamed-Ali-K <kenis.mohamed.ali@gmail.com>
2021-06-27 17:59:20 +0100

    add loading control to booking page add loagic to cancel booking function to booking services

--- src/app/bookings/bookings.page.ts
+++ src/app/bookings/bookings.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { IonItemSliding } from '@ionic/angular';
+import { IonItemSliding, LoadingController } from '@ionic/angular';
 import { Subscription } from 'rxjs';
 
 import { BookingService } from './bookings.service';
@@ -14,7 +14,7 @@ export class BookingsPage implements OnInit, OnDestroy {
   loadedBookings: Booking[];
   private bookingSub: Subscription;
 
-  constructor(private bookingService: BookingService) {}
+  constructor(private bookingService: BookingService, private loadinCtrl: LoadingController) {}
 
   ngOnInit() {
     this.bookingService.bookings.subscribe(bookings => {
@@ -22,9 +22,13 @@ export class BookingsPage implements OnInit, OnDestroy {
     });
   }
 
-  onCancelBooking(offerId: string, slidingEl: IonItemSliding) {
+  onCancelBooking(bookingId: string, slidingEl: IonItemSliding) {
     slidingEl.close();
-    // cancel booking wiht id offerId
+    this.loadinCtrl.create({message: 'Canceling Booking...'}).then(loadingEl =>{
+      loadingEl.present();
+      this.bookingService.cancelBooking(bookingId).subscribe();
+      loadingEl.dismiss();
+    });
   }
 
   ngOnDestroy() {
--- src/app/bookings/bookings.service.ts
+++ src/app/bookings/bookings.service.ts
@@ -46,5 +46,13 @@ export class BookingService {
     );
   }
 
-  cancelBooking(bookingId: string) {}
+  cancelBooking(bookingId: string) {
+    return this.bookings.pipe(
+      take(1),
+      delay(1000),
+      tap(bookings => {
+        this._bookings.next(bookings.filter(b => b.id !== bookingId));
+      })
+    );
+  }
 }
