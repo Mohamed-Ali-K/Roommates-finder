33a5b05ccd9ca160aec896e2ef3828e86b02af77
Mohamed-Ali-K <kenis.mohamed.ali@gmail.com>
2021-06-27 12:18:49 +0100

    add sub to offer page

--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -1,27 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { IonItemSliding } from '@ionic/angular';
 import { Router } from '@angular/router';
 
 import { PlacesService } from '../places.service';
 import { Place } from '../place.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-offers',
   templateUrl: './offers.page.html',
-  styleUrls: ['./offers.page.scss']
+  styleUrls: ['./offers.page.scss'],
 })
-export class OffersPage implements OnInit {
+export class OffersPage implements OnInit, OnDestroy {
   offers: Place[];
-
+  placesSub: Subscription;
   constructor(private placesService: PlacesService, private router: Router) {}
 
   ngOnInit() {
-    this.offers = this.placesService.places;
+    this.placesSub = this.placesService.places.subscribe((places) => {
+      this.offers = places;
+    });
   }
 
   onEdit(offerId: string, slidingItem: IonItemSliding) {
     slidingItem.close();
     this.router.navigate(['/', 'places', 'tabs', 'offers', 'edit', offerId]);
     console.log('Editing item', offerId);
+  }
+
+  ngOnDestroy() {
+    this.placesSub.unsubscribe();
   }
 }
