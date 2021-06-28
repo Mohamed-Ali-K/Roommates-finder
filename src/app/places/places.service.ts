c337af5d3338afe316f5e749bf2ace2b360464f7
Mohamed-Ali-K <kenis.mohamed.ali@gmail.com>
2021-06-28 18:18:22 +0100

    remove dummy places from places services

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -5,6 +5,38 @@ import { Place } from './place.model';
 import { take, map, tap, delay, switchMap } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 
+// Damy places
+// new Place(
+//   'p1',
+//   'Manhattan Mansion',
+//   'In the heart of New York City.',
+//   'https://lonelyplanetimages.imgix.net/mastheads/GettyImages-538096543_medium.jpg?sharp=10&vib=20&w=1200',
+//   149.99,
+//   new Date('2021-01-01'),
+//   new Date('2021-12-31'),
+//   'abc'
+// ),
+// new Place(
+//   'p2',
+//   "L'Amour Toujours",
+//   'A romantic place in Paris!',
+//   'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Paris_Night.jpg/1024px-Paris_Night.jpg',
+//   189.99,
+//   new Date('2021-01-01'),
+//   new Date('2021-12-31'),
+//   'abc'
+// ),
+// new Place(
+//   'p3',
+//   'The Foggy Palace',
+//   'Not your average city trip!',
+//   'https://upload.wikimedia.org/wikipedia/commons/0/01/San_Francisco_with_two_bridges_and_the_fog.jpg',
+//   99.99,
+//   new Date('2021-01-01'),
+//   new Date('2021-12-31'),
+//   'abc'
+// ),
+
 interface PlaceData {
   avilableFrom: string;
   avilableTo: string;
@@ -19,38 +51,7 @@ interface PlaceData {
   providedIn: 'root',
 })
 export class PlacesService {
-  private _places = new BehaviorSubject<Place[]>([
-    new Place(
-      'p1',
-      'Manhattan Mansion',
-      'In the heart of New York City.',
-      'https://lonelyplanetimages.imgix.net/mastheads/GettyImages-538096543_medium.jpg?sharp=10&vib=20&w=1200',
-      149.99,
-      new Date('2021-01-01'),
-      new Date('2021-12-31'),
-      'abc'
-    ),
-    new Place(
-      'p2',
-      "L'Amour Toujours",
-      'A romantic place in Paris!',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Paris_Night.jpg/1024px-Paris_Night.jpg',
-      189.99,
-      new Date('2021-01-01'),
-      new Date('2021-12-31'),
-      'abc'
-    ),
-    new Place(
-      'p3',
-      'The Foggy Palace',
-      'Not your average city trip!',
-      'https://upload.wikimedia.org/wikipedia/commons/0/01/San_Francisco_with_two_bridges_and_the_fog.jpg',
-      99.99,
-      new Date('2021-01-01'),
-      new Date('2021-12-31'),
-      'abc'
-    ),
-  ]);
+  private _places = new BehaviorSubject<Place[]>([]);
   get places() {
     return this._places.asObservable();
   }
