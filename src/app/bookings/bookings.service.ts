fdb9d112960bba590eedb0310fd32b71548c8609
Mohamed-Ali-K <kenis.mohamed.ali@gmail.com>
2021-06-29 14:58:28 +0100

    add http get from fierbase to booking service

--- src/app/bookings/bookings.service.ts
+++ src/app/bookings/bookings.service.ts
@@ -1,9 +1,11 @@
+/* eslint-disable no-underscore-dangle */
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { take, tap, delay } from 'rxjs/operators';
+import { take, tap, delay, switchMap } from 'rxjs/operators';
 
 import { Booking } from './booking.model';
 import { AuthService } from '../auth/auth.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class BookingService {
@@ -13,7 +15,7 @@ export class BookingService {
     return this._bookings.asObservable();
   }
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private http: HttpClient) {}
 
   addBooking(
     placeId: string,
@@ -25,6 +27,7 @@ export class BookingService {
     dateFrom: Date,
     dateTo: Date
   ) {
+    let generatedId: string;
     const newBooking = new Booking(
       Math.random().toString(),
       placeId,
@@ -37,21 +40,30 @@ export class BookingService {
       dateFrom,
       dateTo
     );
-    return this.bookings.pipe(
-      take(1),
-      delay(1000),
-      tap(bookings => {
-        this._bookings.next(bookings.concat(newBooking));
-      })
-    );
+    return this.http
+      .post<{ name: string }>(
+        'https://roommatefinder-23af6-default-rtdb.europe-west1.firebasedatabase.app/booking-places.json',
+        { ...newBooking, id: null }
+      )
+      .pipe(
+        switchMap((resData) => {
+          generatedId = resData.name;
+          return this.bookings;
+        }),
+        take(1),
+        tap((bookings) => {
+          newBooking.id = generatedId;
+          this._bookings.next(bookings.concat(newBooking));
+        })
+      );
   }
 
   cancelBooking(bookingId: string) {
     return this.bookings.pipe(
       take(1),
       delay(1000),
-      tap(bookings => {
-        this._bookings.next(bookings.filter(b => b.id !== bookingId));
+      tap((bookings) => {
+        this._bookings.next(bookings.filter((b) => b.id !== bookingId));
       })
     );
   }
