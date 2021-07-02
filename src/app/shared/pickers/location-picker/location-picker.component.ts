192da0e70c3c663f157a6891e9c8098c76418ca2
Mohamed-Ali-K <kenis.mohamed.ali@gmail.com>
2021-07-02 13:11:26 +0100

    add capasetor geoAction getting the user location

--- src/app/shared/pickers/location-picker/location-picker.component.ts
+++ src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -1,12 +1,18 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import {
+  ActionSheetController,
+  AlertController,
+  ModalController,
+} from '@ionic/angular';
 import { HttpClient } from '@angular/common/http';
 import { map, switchMap } from 'rxjs/operators';
 import { of } from 'rxjs';
 
 import { MapModalComponent } from '../../map-modal/map-modal.component';
 import { environment } from 'src/environments/environment';
-import { PlaceLocation } from 'src/app/places/loaction.model';
+import { Coordinates, PlaceLocation } from 'src/app/places/loaction.model';
+import { Capacitor } from '@capacitor/core';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-location-picker',
@@ -18,13 +24,70 @@ export class LocationPickerComponent implements OnInit {
   selectedLocationImage: string;
   isLoading = false;
 
-  constructor(private modalCtrl: ModalController, private http: HttpClient) {}
+  constructor(
+    private modalCtrl: ModalController,
+    private http: HttpClient,
+    private actionSheetCtrl: ActionSheetController,
+    private alertCtrl: AlertController
+  ) {}
 
   ngOnInit() {}
 
   onPickLocation() {
+    this.actionSheetCtrl
+      .create({
+        header: 'Please Choose',
+        buttons: [
+          {
+            text: 'Auto-Locate',
+            handler: () => {
+              this.locateUser();
+            },
+          },
+          {
+            text: 'Pick on Map',
+            handler: () => {
+              this.openMap();
+            },
+          },
+          { text: 'Cancel', role: 'cancel' },
+        ],
+      })
+      .then((actionSheetEl) => {
+        actionSheetEl.present();
+      });
+  }
+  private locateUser() {
+    if (!Capacitor.isPluginAvailable('Geolocation')) {
+      this.showErrorAlert();
+      return;
+    }
+    Geolocation.getCurrentPosition()
+      .then((geoPosition) => {
+        const coordinats: Coordinates = {
+          lat: geoPosition.coords.altitude,
+          lng: geoPosition.coords.longitude,
+        };
+      })
+      .catch((err) => {
+        this.showErrorAlert();
+      });
+  }
+
+  private showErrorAlert() {
+    this.alertCtrl
+      .create({
+        header: 'Could not fetch Location',
+        message: 'Please use the Map to pick a Location!',
+      })
+      .then((alertEl) => {
+        alertEl.present();
+      });
+  }
+
+  private openMap() {
     this.modalCtrl.create({ component: MapModalComponent }).then((modalEl) => {
-      modalEl.onDidDismiss().then(modalData => {
+      modalEl.onDidDismiss().then((modalData) => {
         if (!modalData.data) {
           return;
         }
@@ -32,19 +95,19 @@ export class LocationPickerComponent implements OnInit {
           lat: modalData.data.lat,
           lng: modalData.data.lng,
           address: null,
-          staticMapImageUrl: null
+          staticMapImageUrl: null,
         };
         this.isLoading = true;
         this.getAddress(modalData.data.lat, modalData.data.lng)
           .pipe(
-            switchMap(address => {
+            switchMap((address) => {
               pickedLocation.address = address;
               return of(
                 this.getMapImage(pickedLocation.lat, pickedLocation.lng, 14)
               );
             })
           )
-          .subscribe(staticMapImageUrl => {
+          .subscribe((staticMapImageUrl) => {
             pickedLocation.staticMapImageUrl = staticMapImageUrl;
             this.selectedLocationImage = staticMapImageUrl;
             this.isLoading = false;
@@ -58,12 +121,10 @@ export class LocationPickerComponent implements OnInit {
   private getAddress(lat: number, lng: number) {
     return this.http
       .get<any>(
-        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${
-          environment.googleMapAPIKey
-        }`
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${environment.googleMapAPIKey}`
       )
       .pipe(
-        map(geoData => {
+        map((geoData) => {
           if (!geoData || !geoData.results || geoData.results.length === 0) {
             return null;
           }
